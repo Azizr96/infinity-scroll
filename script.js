02bdf6c61d4bddc25ac8065c33263cdd548d0999
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-06-20 13:01:49 +0100

    create element links to add to DOM

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 let photoArray = [];
 
+// create Elements for links & photos and add to dom
+
+function displayPhotos(){
+  photoArray.forEach((photo) => {
+    // create <a> to link to unsplash
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
+    // create <img> per photo
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+    //put image <img> inside the <a>, then put both inside image container element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 
 //  Get photos for Unsplash APi
